e44bf3a617936f74821b3e343c42a7a7132d712d
Elf <tokhtarkhan1967@gmail.com>
2024-09-22 16:12:02 +0500

    re_path for hm

--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path("task/new/", views.TaskCreateView.as_view(), name="task_create"),
     path("task/<int:pk>/edit/", views.TaskUpdateView.as_view(), name="task_edit"),
     path("task/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task_delete"),
+    re_path(
+        r"^task/(?P<pk>\d+)/$", views.TaskDetailView.as_view(), name="task_detail_regex"
+    ),
 ]
